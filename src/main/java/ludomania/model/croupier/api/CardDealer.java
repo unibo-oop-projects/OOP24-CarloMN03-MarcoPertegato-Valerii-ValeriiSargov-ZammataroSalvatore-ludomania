daa32c530ac643a2aa35bc2fd0e09648ca38b980
CarloMN03 <carlo.nicastro2@studio.unibo.it>
2025-04-15 15:43:04 +0200

    Updated CardDealer
    Added all necessary methods to  abstract class CardDealer in order to make BlackJack and TrenteEtQuarante work
    
    Co-Authored-By: Marco Pertegato <186720179+perte-ma@users.noreply.github.com>

--- src/main/java/ludomania/model/croupier/api/CardDealer.java
+++ src/main/java/ludomania/model/croupier/api/CardDealer.java
@@ -1,6 +1,58 @@
 package ludomania.model.croupier.api;
-/*
- * public interface CardDealer extends Croupier {
- * 
- * }
- */
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Random;
+
+import io.lyuda.jcards.Card;
+import io.lyuda.jcards.Deck;
+import io.lyuda.jcards.DeckFactory;
+import ludomania.model.bet.api.Bet;
+import ludomania.model.player.api.Player;
+
+public abstract class CardDealer<T> extends Croupier<T> {
+
+    private final DeckFactory decks;
+
+    public CardDealer(Map<Player, Bet> roundBet, DeckFactory decks) {
+        super(roundBet);
+        this.decks = decks;
+    }
+
+    public int getDeckCount() {
+        return decks.getDeckCount();
+    }
+
+    //Probabilmente inutilizzata
+    public Deck getSingleDeck(int index) {
+        return decks.getDeck(index);
+    }
+
+    public void shuffleAll() {
+        decks.shuffleAllDecks();
+    }
+    
+    public void initDeck(int amount) {
+        for (int i = 0; i < amount; i++) {
+            decks.createDeck();
+        }
+    }
+
+    public void removeDeck(int index) {
+        decks.removeDeck(index);
+    }
+
+    public Card drawCard() {
+        Random rand = new Random();
+        int index = rand.nextInt(getDeckCount());
+        while(decks.getDeck(index).getCards().isEmpty()) {
+            decks.removeDeck(index);
+            index = rand.nextInt(getDeckCount());
+        }
+        return decks.getDeck(index).deal();
+    }
+
+    public boolean needToResetAllDecks() {
+        return decks.getDeckCount() < 3;
+    }
+}
