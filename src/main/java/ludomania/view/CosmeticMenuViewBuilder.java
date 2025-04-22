e93a14a64dc09a3d4de86485b03268f1f849df27
Valerii Sargov <valerii.sargov@studio.unibo.it>
2025-04-22 18:16:18 +0200

    Dryrefactoring of the cosmeticMenuviewbuilder
    Dry refactoring since there were three section method that had repeated code

--- src/main/java/ludomania/view/CosmeticMenuViewBuilder.java
+++ src/main/java/ludomania/view/CosmeticMenuViewBuilder.java
@@ -1,10 +1,10 @@
 package ludomania.view;
 
 import java.util.List;
+import java.util.function.Function;
 
 import io.lyuda.jcards.Rank;
 import io.lyuda.jcards.Suit;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -12,18 +12,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import ludomania.core.api.ImageProvider;
 import ludomania.core.api.LanguageManager;
-import ludomania.cosmetics.BackgroundTheme;
-import ludomania.cosmetics.CardTheme;
-import ludomania.cosmetics.FicheTheme;
+import ludomania.cosmetics.CosmeticTheme;
 import ludomania.handler.CosmeticMenuHandler;
 
 public class CosmeticMenuViewBuilder implements ViewBuilder {
@@ -32,7 +27,8 @@ public class CosmeticMenuViewBuilder implements ViewBuilder {
     private final LanguageManager languageManager;
     private final ImageProvider imageProvider;
 
-    public CosmeticMenuViewBuilder(final CosmeticMenuHandler eventHandler, final LanguageManager languageManager,
+    public CosmeticMenuViewBuilder(final CosmeticMenuHandler eventHandler,
+            final LanguageManager languageManager,
             final ImageProvider imageProvider) {
         this.eventHandler = eventHandler;
         this.languageManager = languageManager;
@@ -41,102 +37,99 @@ public class CosmeticMenuViewBuilder implements ViewBuilder {
 
     @Override
     public Parent build() {
-        VBox result = new VBox(10, titleSection(), backgroundSelectionSection(), cardSelectionSection(),
-                ficheSelectionSection(), goBackSection());
+        VBox result = new VBox(10,
+                titleSection(),
+                backgroundSelectionSection(),
+                cardSelectionSection(),
+                ficheSelectionSection(),
+                goBackSection());
         result.setAlignment(Pos.CENTER);
-        result.setBackground(new Background(new BackgroundFill(
-                imageProvider.getBackgroundColor(),
-                CornerRadii.EMPTY,
-                Insets.EMPTY)));
         return result;
     }
 
     private Node titleSection() {
-        Label result = new Label(languageManager.getString("shop"));
-        return result;
+        return new Label(languageManager.getString("shop"));
     }
 
     private Node backgroundSelectionSection() {
-        HBox result = new HBox(
-                selectionBackgroundGroup(eventHandler.getBackgroundThemes()));
-        result.setAlignment(Pos.BASELINE_CENTER);
-        return result;
+        return createSelectionSection(
+                eventHandler.getBackgroundThemes(),
+                eventHandler.getSelectedBackgroundTheme(),
+                theme -> new Rectangle(100, 100, Color.web(theme.getCosmetic())),
+                eventHandler::handleBackgroundChange);
     }
 
     private Node cardSelectionSection() {
-        HBox result = new HBox(
-                selectionCardGroup(eventHandler.getCardThemes()));
-        result.setAlignment(Pos.BASELINE_CENTER);
-        result.setMaxHeight(100);
-        return result;
+        return createSelectionSection(
+                eventHandler.getCardThemes(),
+                eventHandler.getSelectedCardTheme(),
+                theme -> imageProvider.svgHelperMethod(theme.getCosmetic(Rank.ACE, Suit.CLUBS)),
+                eventHandler::handleCardChange);
     }
 
     private Node ficheSelectionSection() {
-        HBox result = new HBox(
-                selectionFicheGroup(eventHandler.getFicheThemes()));
-        result.setAlignment(Pos.BASELINE_CENTER);
-        return result;
+        return createSelectionSection(
+                eventHandler.getFicheThemes(),
+                eventHandler.getSelectedFicheTheme(),
+                theme -> imageProvider.svgHelperMethod(theme.getCosmetic(FICHE_VALUE)),
+                eventHandler::handleFicheChange);
     }
 
-    private Node selectionFicheGroup(List<FicheTheme> cosmetics) {
-        ToggleGroup selectableCosmetics = new ToggleGroup();
-        HBox result = new HBox();
-        for (FicheTheme theme : cosmetics) {
-            ToggleButton cosmeticButton = new ToggleButton();
-            String svg = theme.getCosmetic(FICHE_VALUE);
-            cosmeticButton.setUserData(theme.getTheme());
-            cosmeticButton.setGraphic(imageProvider.svgHelperMethod(svg));
-            cosmeticButton.setToggleGroup(selectableCosmetics);
-            result.getChildren().add(cosmeticButton);
-        }
-        selectableCosmetics.selectedToggleProperty().addListener((obs, oldSelectedToggle, newSelectedToggle) -> {
-            if (newSelectedToggle != null) {
-                eventHandler.handleFicheChange(newSelectedToggle.getUserData().toString());
+    private <T> Node createSelectionSection(List<T> items,
+            CosmeticTheme selectedTheme,
+            Function<T, Node> graphicCreator,
+            java.util.function.Consumer<T> changeHandler) {
+        ToggleGroup toggleGroup = new ToggleGroup();
+        HBox container = new HBox();
+        container.setAlignment(Pos.BASELINE_CENTER);
+
+        items.forEach(item -> {
+            ToggleButton button = createThemeToggleButton(
+                    item,
+                    selectedTheme,
+                    graphicCreator,
+                    toggleGroup);
+            container.getChildren().add(button);
+        });
+
+        toggleGroup.selectedToggleProperty().addListener((obs, old, newToggle) -> {
+            if (newToggle != null) {
+                changeHandler.accept((T) newToggle.getUserData());
             }
         });
-        return result;
+
+        return container;
     }
 
-    private Node selectionCardGroup(List<CardTheme> cosmetics) {
-        ToggleGroup selectableCosmetics = new ToggleGroup();
-        HBox result = new HBox();
-        for (CardTheme theme : cosmetics) {
-            ToggleButton cosmeticButton = new ToggleButton();
-            String svg = theme.getCosmetic(Rank.ACE, Suit.CLUBS);
-            cosmeticButton.setUserData(theme.getTheme());
-            cosmeticButton.setGraphic(imageProvider.svgHelperMethod(svg));
-            cosmeticButton.setToggleGroup(selectableCosmetics);
-            result.getChildren().add(cosmeticButton);
+    private <T> ToggleButton createThemeToggleButton(T item,
+            CosmeticTheme selectedTheme,
+            Function<T, Node> graphicCreator,
+            ToggleGroup toggleGroup) {
+        ToggleButton button = new ToggleButton();
+
+        if (isSelectedTheme(item, selectedTheme)) {
+            button.setSelected(true);
         }
-        selectableCosmetics.selectedToggleProperty().addListener((obs, oldSelectedToggle, newSelectedToggle) -> {
-            if (newSelectedToggle != null) {
-                System.out.println(newSelectedToggle.getUserData());
-            }
-        });
-        return result;
+
+        button.setUserData(item);
+        button.setGraphic(graphicCreator.apply(item));
+        button.setToggleGroup(toggleGroup);
+
+        return button;
     }
 
-    private Node selectionBackgroundGroup(List<BackgroundTheme> cosmetics) {
-        ToggleGroup selectableCosmetics = new ToggleGroup();
-        HBox result = new HBox();
-        for (BackgroundTheme theme : cosmetics) {
-            ToggleButton cosmeticButton = new ToggleButton();
-            String color = theme.getCosmetic();
-            cosmeticButton.setUserData(theme.getTheme());
-            cosmeticButton.setGraphic(new Rectangle(100, 100, Color.web(color)));
-            cosmeticButton.setToggleGroup(selectableCosmetics);
-            result.getChildren().add(cosmeticButton);
+    private <T> boolean isSelectedTheme(T item, CosmeticTheme selectedTheme) {
+        try {
+            // Using reflection to call getTheme() on different theme types
+            return item.getClass().getMethod("getTheme").invoke(item).equals(selectedTheme);
+        } catch (Exception e) {
+            return false;
         }
-        selectableCosmetics.selectedToggleProperty().addListener((obs, oldSelectedToggle, newSelectedToggle) -> {
-            if (newSelectedToggle != null) {
-                System.out.println(newSelectedToggle.getUserData());
-            }
-        });
-        return result;
     }
 
     private Node goBackSection() {
-        Button result = new Button(languageManager.getString("go_back_button"));
-        return result;
+        Button button = new Button(languageManager.getString("go_back_button"));
+        button.setOnMouseClicked(e -> eventHandler.handleBack());
+        return button;
     }
 }
