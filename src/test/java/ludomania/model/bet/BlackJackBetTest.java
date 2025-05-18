9441d1a671a35b401eacb4cc7c61da27a88b50d0
CarloMN03 <carlo.nicastro2@studio.unibo.it>
2025-05-18 11:39:47 +0200

    BlackJack bets and testing
    Create blackjack bet and betType classes and test these classes

---
+++ src/test/java/ludomania/model/bet/BlackJackBetTest.java
@@ -0,0 +1,25 @@
+package ludomania.model.bet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import ludomania.model.bet.impl.BlackJackBetType;
+
+public class BlackJackBetTest {
+    
+    @Test
+    public void TestTypeNameAndPayout() {
+        // Test BASE
+        assertEquals("Base", BlackJackBetType.BASE.getTypeName());
+        assertEquals(1.0, BlackJackBetType.BASE.getPayout());
+
+        // Test DOUBLE
+        assertEquals("Double", BlackJackBetType.DOUBLE.getTypeName());
+        assertEquals(2.0, BlackJackBetType.DOUBLE.getPayout());
+
+        // Test BLACKJACK
+        assertEquals("BlackJack", BlackJackBetType.BLACKJACK.getTypeName());
+        assertEquals(1.5, BlackJackBetType.BLACKJACK.getPayout());
+    }
+
+}
