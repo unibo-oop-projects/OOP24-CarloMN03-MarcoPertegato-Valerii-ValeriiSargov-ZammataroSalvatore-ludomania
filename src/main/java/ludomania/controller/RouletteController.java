01018a5179ea29e25fbc7d1f7b9556b7b007f54a
Zammataro Salvatore <zammatarosalvatore99@gmail.com>
2025-06-17 23:30:51 +0200

    roulette
    update UI event listening
    update UI style

--- src/main/java/ludomania/controller/RouletteController.java
+++ src/main/java/ludomania/controller/RouletteController.java
@@ -16,209 +16,121 @@ public class RouletteController implements Controller {
     private final Builder<Parent> viewBuilder;
     private final SceneManager sceneManager;
     private final AudioManager audioManager;
-
+    
     public RouletteController(
-        final SceneManager sceneManager,
-        final AudioManager audioManager
+    final SceneManager sceneManager,
+    final AudioManager audioManager
     ) {
         this.sceneManager = sceneManager;
         this.audioManager = audioManager;
         viewBuilder = new RouletteViewBuilder(this, sceneManager.getLanguageManager(),
         sceneManager.getImageProvider());
     }
-
+    
     @Override
     public Parent getView() {
         return viewBuilder.build();
     }
-
+    
     @FXML
     public void pleinBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
             int id = Integer.parseInt(clickedButton.getId());
-
-            // 4. Get the button's CSS style classes
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + id);
         } else {
             System.out.println("Event source is not a Button.");
         }
-
     }
     
     @FXML
     public void chevalBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Separator) {
             Separator clickedButton = (Separator) source;
-
-            // 3. Get the button's text
+            
             String[] ids = clickedButton.getId().split("-");
-
-            // 4. Get the button's CSS style classes
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + ids.toString());
         } else {
             System.out.println("Event source is not a Button.");
-        }
-
+        }    
     }
     
     @FXML
     public void carreBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
             String[] ids = clickedButton.getId().split("-");
-
-            // 4. Get the button's CSS style classes
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + ids.toString());
         } else {
             System.out.println("Event source is not a Button.");
         }
-
     }
     
     @FXML
     public void colonneBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
             Character id = clickedButton.getId().charAt(0);
-
-            // 4. Get the button's CSS style classes
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + id);
         } else {
             System.out.println("Event source is not a Button.");
         }
-
     }
     
     @FXML
     public void noirBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
             String id = clickedButton.getId();
-
-            // 4. Get the button's CSS style classes
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + id);
         } else {
             System.out.println("Event source is not a Button.");
         }
-
     }
     
     @FXML
     public void rougeBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
             String id = clickedButton.getId();
-
-            // 4. Get the button's CSS style classes
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + id);
         } else {
             System.out.println("Event source is not a Button.");
         }
     }
-
+    
     @FXML
     public void pairBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
             String id = clickedButton.getId();
-
-            // 4. Get the button's CSS style classes
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + id);
         } else {
             System.out.println("Event source is not a Button.");
         }
@@ -226,26 +138,14 @@ public class RouletteController implements Controller {
     
     @FXML
     public void impairBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
             String id = clickedButton.getId();
-
-            // 4. Get the button's CSS style classes
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + id);
         } else {
             System.out.println("Event source is not a Button.");
         }
@@ -253,26 +153,14 @@ public class RouletteController implements Controller {
     
     @FXML
     public void passeBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
             String id = clickedButton.getId();
-
-            // 4. Get the button's CSS style classes
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + id);
         } else {
             System.out.println("Event source is not a Button.");
         }
@@ -280,26 +168,14 @@ public class RouletteController implements Controller {
     
     @FXML
     public void manqueBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
             String id = clickedButton.getId();
-
-            // 4. Get the button's CSS style classes
-            ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + id);
+            ObservableList<String> styleClasses = clickedButton.getStyleClass();    
         } else {
             System.out.println("Event source is not a Button.");
         }
@@ -307,30 +183,46 @@ public class RouletteController implements Controller {
     
     @FXML
     public void douzineBet(MouseEvent event) {
-        System.out.println("Button clicked");
-
-        // 1. Get the source of the event
         Object source = event.getSource();
-
-        // 2. Check if the source is a Button and cast it
+        
         if (source instanceof Button) {
             Button clickedButton = (Button) source;
-
-            // 3. Get the button's text
+            
             String buttonText = clickedButton.getText();
-            Character id = clickedButton.getId().charAt(0);
-
-            // 4. Get the button's CSS style classes
+            Character id = clickedButton.getId().charAt(0);            
             ObservableList<String> styleClasses = clickedButton.getStyleClass();
-
-            System.out.println("Button Clicked!");
-            System.out.println("  Text: " + buttonText);
-            System.out.println("  Classes: " + styleClasses);
-            System.out.println("  id: " + id);
         } else {
             System.out.println("Event source is not a Button.");
         }
     }
-
-
+    
+    @FXML
+    void highlightCarre(MouseEvent event) {
+        Object source = event.getSource();
+        
+        if (source instanceof Button) {
+            Button clickedButton = (Button) source;
+            clickedButton.getParent().setStyle("-fx-border-color: #00eeff; -fx-border-width: 2px;");
+            
+            String buttonText = clickedButton.getText();
+            String[] ids = clickedButton.getId().split("-");
+        } else {
+            System.out.println("Event source is not a Button.");
+        }
+    }
+    
+    @FXML
+    void unhighlightCarre(MouseEvent event) {
+        Object source = event.getSource();
+        
+        if (source instanceof Button) {
+            Button clickedButton = (Button) source;
+            clickedButton.getParent().setStyle("-fx-border-color: trasparent; -fx-border-width: 1px;");
+            
+            String buttonText = clickedButton.getText();
+            String[] ids = clickedButton.getId().split("-");
+        } else {
+            System.out.println("Event source is not a Button.");
+        }
+    }
 }
