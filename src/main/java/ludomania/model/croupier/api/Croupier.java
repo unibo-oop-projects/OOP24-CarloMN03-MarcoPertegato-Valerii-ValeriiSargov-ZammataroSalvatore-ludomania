26f07f854bea45f40182bfc5be7de9df63cdab1c
Zammataro Salvatore <zammatarosalvatore99@gmail.com>
2025-06-18 21:28:40 +0200

    update Croupier interface

--- src/main/java/ludomania/model/croupier/api/Croupier.java
+++ src/main/java/ludomania/model/croupier/api/Croupier.java
@@ -1,27 +1,29 @@
 package ludomania.model.croupier.api;
 
+import java.util.List;
 import java.util.Map;
 
+import ludomania.model.Pair;
 import ludomania.model.bet.api.Bet;
 import ludomania.model.game.api.CounterResult;
 import ludomania.model.player.api.Player;
 
 public abstract class Croupier<T> {
-    private final Map<Player, Bet> roundBet;
+    private final List<Pair<Player,Bet>> roundBet;
 
-    public Croupier(final Map<Player, Bet> roundBet) {
+    public Croupier(final List<Pair<Player,Bet>> roundBet) {
         this.roundBet = roundBet;
     }
 
     public void addBet(final Player player, final Bet bet) {
-        roundBet.put(player, bet);
+        roundBet.add(new Pair<>(player, bet));
     }
 
     public void clearRound() {
         roundBet.clear();
     }
 
-    public Map<Player, Bet> getRoundBet() {
+    public List<Pair<Player,Bet>> getRoundBet() {
         return roundBet;
     }
 
