72bacbc5938133381b2f9b712c2ea19d81b50e19
Zammataro Salvatore <zammatarosalvatore99@gmail.com>
2025-06-21 22:12:02 +0200

    update roulette croupier test

--- src/test/java/ludomania/model/croupier/RouletteCroupierTest.java
+++ src/test/java/ludomania/model/croupier/RouletteCroupierTest.java
@@ -3,16 +3,17 @@ package ludomania.model.croupier;
 import ludomania.model.Pair;
 import ludomania.model.bet.RouletteBet;
 import ludomania.model.bet.RouletteBetFactory;
-import ludomania.model.bet.RouletteBetType;
 import ludomania.model.croupier.roulette.RouletteColor;
 import ludomania.model.croupier.roulette.RouletteCroupier;
 import ludomania.model.game.impl.CounterResult;
 import ludomania.model.player.RoulettePlayer;
+import ludomania.model.player.api.Player;
+import ludomania.model.wallet.impl.WalletImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,8 +21,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DisplayName("Roulette Wheel Methods Test")
 public class RouletteCroupierTest {
-    private Integer _22 = 22;
-    private CounterResult<Pair<Integer, RouletteColor>> mockResult22 =
+    private final Integer _22 = 22;
+    private final CounterResult<Pair<Integer, RouletteColor>> mockResult22 =
             new CounterResult<>(new Pair<>(22, RouletteColor.NOIR));
 
     private Set<Object> mockChoice;
@@ -31,9 +32,10 @@ public class RouletteCroupierTest {
 
     @BeforeEach
     void setUp() {
-        mockChoice = new HashSet<>(this._22);
+        mockChoice = Set.of(this._22);
         mockCroupier = new RouletteCroupier();
         mockAmount = 10.0;
+        this.mockPlayer = new RoulettePlayer(new WalletImpl(1000.0), "TestPlayer");
     }
 
     @Test
@@ -41,33 +43,39 @@ public class RouletteCroupierTest {
         final RouletteBet mockPleinBet = RouletteBetFactory.pleinBet(this.mockChoice, this.mockAmount);
         this.mockCroupier.addBet(this.mockPlayer, mockPleinBet);
 
-        this.mockCroupier.checkBets(this.mockResult22);
-        
+        final Map<Player, Double> winningBets = this.mockCroupier.checkBets(this.mockResult22);
+
+        assertTrue(winningBets.containsKey(this.mockPlayer));
+        assertEquals(mockPleinBet.evaluate(), winningBets.get(this.mockPlayer));
     }
 
-//    checkBets() {
-//        final Set<Object> mockChoice = Set.of(23);
-//        final RouletteBet bet = RouletteBetFactory.pleinBet(mockChoice, mockAmount);
-//
-//        assertNotNull(bet);
-//        assertEquals(mockAmount, bet.getValue());
-//        assertEquals(mockChoice, bet.getChoice());
-//        assertEquals(RouletteBetType.PLEIN, bet.getType());
-//
-//        assertTrue(bet.success.apply(new Pair<>(23, RouletteColor.ROUGE), bet.getChoice()));
-//
-//        assertFalse(bet.success.apply(new Pair<>(25, RouletteColor.ROUGE), bet.getChoice()));
-//    }
-//
-//    getRoundBet() {
-//
-//    }
-//
-//    clearRound() {
-//
-//    }
-//
-//    addBet() {
-//
-//    }
+    @Test
+    void getRoundBet() {
+        final RouletteBet mockPleinBet = RouletteBetFactory.pleinBet(this.mockChoice, this.mockAmount);
+        this.mockCroupier.addBet(this.mockPlayer, mockPleinBet);
+
+        assertEquals(1, this.mockCroupier.getRoundBet().size());
+        assertEquals(1, this.mockCroupier.getRoundBet()
+                        .stream()
+                        .filter(b -> b.getKey().equals(mockPlayer) && b.getValue().equals(mockPleinBet))
+                        .count());
+    }
+
+    @Test
+    void addBet() {
+        final RouletteBet mockPleinBet = RouletteBetFactory.pleinBet(this.mockChoice, this.mockAmount);
+        this.mockCroupier.addBet(this.mockPlayer, mockPleinBet);
+
+        assertFalse(this.mockCroupier.getRoundBet().isEmpty());
+    }
+
+    @Test
+    void clearRound() {
+        final RouletteBet mockPleinBet = RouletteBetFactory.pleinBet(this.mockChoice, this.mockAmount);
+        this.mockCroupier.addBet(this.mockPlayer, mockPleinBet);
+
+        this.mockCroupier.clearRound();
+        assertTrue(this.mockCroupier.getRoundBet().isEmpty());
+    }
+
 }
